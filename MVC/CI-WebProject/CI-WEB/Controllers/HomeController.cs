0c6ca58c940125f9db17507dadf50b0968264778
ozakaushal <kaushaloza48@gmail.com>
2023-02-22 21:48:26 +0530

    assignment done till volunteering page

--- MVC/CI-WebProject/CI-WEB/Controllers/HomeController.cs
+++ MVC/CI-WebProject/CI-WEB/Controllers/HomeController.cs
@@ -42,6 +42,11 @@ namespace Web_Pages.Controllers
             return View();
         }
 
+        public IActionResult MissionDetails()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
